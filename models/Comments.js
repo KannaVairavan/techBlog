df472d1bb3994a0c85703225948e19cca883cbe0
Kannathal Vairavan <kannamanickam@gmail.com>
2021-04-06 21:31:39 -0500

    Added comments.js model

---
+++ models/Comments.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model{}
+
+Comments.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+        comment:{
+            type:DataTypes.STRING,
+        },
+        user_id:{
+            type:DataTypes.INTEGER,
+            references:{
+                model:'user',
+                key:'id',
+            }
+        },
+        post_id:{
+            type:DataTypes.INTEGER,
+            references:{
+                model:'post',
+                key:'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps:false,
+        freezeTableName:false,
+        underscored:true,
+        modelName:'comments',
+    }
+
+);
+
+module.exports=Comments;
+
